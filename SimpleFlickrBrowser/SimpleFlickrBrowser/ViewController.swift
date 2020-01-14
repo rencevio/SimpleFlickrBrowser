48c80cd90276f6db2f07b51bde339f453fd575fa
Maxim Berezhnoy <berezhnoy.maxim@gmail.com>
2020-01-14 15:46:12 +0100

    Initial project setup + gitignore

---
+++ SimpleFlickrBrowser/SimpleFlickrBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleFlickrBrowser
+//
+//  Created by maxim on 14/01/2020.
+//  Copyright © 2020 rencevio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
