ed099bc00ca0b8c9c22eb3362e2f12ca4345ff83
Maxim Berezhnoy <berezhnoy.maxim@gmail.com>
2020-01-17 19:28:14 +0100

    Placeholder and image view for browser cell

---
+++ SimpleFlickrBrowser/SimpleFlickrBrowser/Scenes/Browser/Cell/BrowserCellView.swift
@@ -0,0 +1,116 @@
+//
+// Created by Maxim Berezhnoy on 16/01/2020.
+//
+// Copyright (c) 2020 rencevio. All rights reserved.
+
+import UIKit
+
+protocol BrowserCellDisplaying: class {
+    func display(image: PhotoImage.ViewModel)
+}
+
+final class BrowserViewCell: UICollectionViewCell {
+    private enum ViewState {
+        case loading
+        case image(UIImage)
+    }
+
+    static let identifier = "\(BrowserViewCell.self)"
+
+    private var interactor: BrowserCellInteracting?
+    private var photo: Photo?
+
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+
+        view.contentMode = .scaleAspectFit
+
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 7
+
+        return view
+    }()
+
+    lazy var placeholderView: UIView = {
+        let view = UIView()
+        
+        let label = UILabel(frame: .zero)
+        
+        label.text = "Placeholder"
+        label.textAlignment = .center
+        
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        label.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupImageView()
+        setupPlaceholderView()
+
+        set(state: .loading)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupImageView() {
+        addSubview(imageView)
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+    }
+
+    private func setupPlaceholderView() {
+        addSubview(placeholderView)
+
+        placeholderView.translatesAutoresizingMaskIntoConstraints = false
+        placeholderView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        placeholderView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        placeholderView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        placeholderView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+    }
+
+    private func set(state: ViewState) {
+        switch state {
+        case .loading:
+            placeholderView.isHidden = false
+            imageView.isHidden = true
+
+        case .image(let image):
+            imageView.isHidden = false
+            placeholderView.isHidden = true
+
+            imageView.image = image
+        }
+    }
+}
+
+// MARK: - BrowserCellDisplaying
+extension BrowserViewCell: BrowserCellDisplaying {
+    func display(image: PhotoImage.ViewModel) {
+        set(state: .image(image.image))
+    }
+}
+
+// MARK: - ConfigurableBrowserCell
+extension BrowserViewCell: ConfigurableBrowserCell {
+    func configure(interactor: BrowserCellInteracting, photo: Photo) {
+        self.interactor = interactor
+        self.photo = photo
+
+        interactor.fetch(image: PhotoImage.Request(url: photo.image))
+    }
+}
